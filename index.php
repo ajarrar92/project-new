8a65c2b66d4a88fad3a33da757acbb51c2a23366
Ahmad Jarrar <ajarrar@blulogix.com>
2017-10-05 14:45:33 +0300

    add file

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang="en">
+    
+  <head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet"> 
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script src ="js/angular.min.js"></script>
+  </head>
+
+  <body>
+
+    
+     <script src = "js/script.js"></script>
+  </body>
+</html>
+
+
+
